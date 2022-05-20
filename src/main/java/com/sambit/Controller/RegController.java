b1733be7f0d58d21d05cfbfc732861afa18f0681
Sambit Kumar Pradhan <48365115+psambitkumar@users.noreply.github.com>
2022-05-20 11:35:03 +0530

    Get Bank Details Using IFSC Code By Api Call

--- src/main/java/com/sambit/Controller/RegController.java
+++ src/main/java/com/sambit/Controller/RegController.java
@@ -1,5 +1,6 @@
 package com.sambit.Controller;
 
+import com.google.gson.Gson;
 import com.sambit.Bean.*;
 import com.sambit.Entity.*;
 import com.sambit.Repository.PostalRepository;
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -355,6 +357,20 @@ public class RegController {
         SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
         Date date = formatter.parse(date_string);
         DaysBetweenDates.daysCountBetweenDates(date);
+        return null;
+    }
+
+//    IFSC Code Check API Call
+    @GetMapping("/checkIFSC")
+    public String checkIFSC(){
+        Gson gson = new Gson();
+        String ifscCode ="SBIN0003942";
+        RestTemplate restTemplate = new RestTemplate();
+        String bankDetails = restTemplate.getForObject("https://ifsc.razorpay.com/"+ifscCode, String.class);
+//        Converting JSON String to ModeL Class
+        BankDetailsBean bankDetailsBean = gson.fromJson(bankDetails, BankDetailsBean.class);
+        System.out.println("Bank Details are : " + bankDetailsBean);
+        System.out.println(bankDetails);
         return null;
     }
 
