a9edecd1941b15045fa3dab2be7f748344db2717
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-10-13 14:45:59 +0530

    Weakly Preparation Kit

---
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/WeekPreparationKit/PlusMinus.java
@@ -0,0 +1,30 @@
+package com.sambit.CompetitvePractice.HackerRank.WeekPreparationKit;
+
+import java.util.List;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 13/10/2022 - 12:28 PM
+ */
+public class PlusMinus {
+	public static void plusMinus(List<Integer> arr){
+		int positive = 0, negative = 0, zero = 0;
+		for (Integer integer : arr) {
+			if (integer > 0) {
+				positive++;
+			} else if (integer < 0) {
+				negative++;
+			} else {
+				zero++;
+			}
+		}
+		System.out.println((double)positive/arr.size());
+		System.out.println((double)negative/arr.size());
+		System.out.println((double)zero/arr.size());
+	}
+
+	public static void main(String[] args) {
+		plusMinus(List.of(-4, 3, -9, 0, 4, 1));
+	}
+}
---
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/WeekPreparationKit/TimeConversion.java
@@ -0,0 +1,34 @@
+package com.sambit.CompetitvePractice.HackerRank.WeekPreparationKit;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 13/10/2022 - 1:49 PM
+ */
+public class TimeConversion {
+	public static String timeConversion(String s) {
+		String[] time = s.split(":");
+		String hour = time[0];
+		String minute = time[1];
+		String second = time[2].substring(0, 2);
+		String amPm = time[2].substring(2, 4);
+		if (amPm.equals("PM")) {
+			if (hour.equals("12")) {
+				hour = "12";
+			} else {
+				hour = String.valueOf(Integer.parseInt(hour) + 12);
+			}
+		} else {
+			if (hour.equals("12")) {
+				hour = "00";
+			} else {
+				hour = hour;
+			}
+		}
+		return hour + ":" + minute + ":" + second;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(timeConversion("12:45:54PM"));
+	}
+}
