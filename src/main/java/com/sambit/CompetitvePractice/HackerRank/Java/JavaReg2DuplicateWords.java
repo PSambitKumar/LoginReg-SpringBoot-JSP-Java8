96573ef63f71b5dbdd908f13d9faec7cb4cfe18c
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-10-20 12:38:37 +0530

    Maximum Path Sum 1 Hackerrank Java Solution

--- src/main/java/com/sambit/CompetitvePractice/HackerRank/Java/JavaReg2DuplicateWords.java
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/Java/JavaReg2DuplicateWords.java
@@ -13,18 +13,16 @@ public class JavaReg2DuplicateWords {
 	public static void main(String[] args) {
 		String regex = "\\b(\\w+)(\\b\\W+\\b\\1\\b)*";
 		Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-
 		Scanner in = new Scanner(System.in);
 		int numSentences = Integer.parseInt(in.nextLine());
 		while (numSentences-- > 0) {
 			String input = in.nextLine();
 			Matcher m = p.matcher(input);
+			// Check for subsequences of input that match the compiled pattern
 			while (m.find()) {
 				input = input.replaceAll(m.group(), m.group(1));
 			}
-			while (m.find()) {
-				input = input.replaceAll(m.group(), m.group(1));
-			}
+			// Prints the modified sentence.
 			System.out.println(input);
 		}
 		in.close();
