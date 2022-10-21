ec9213cd8da6dac80442a72bae5be37ef824ea3c
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-10-21 11:40:54 +0530

    Project Euler 19, Counting Sundays

--- src/main/java/com/sambit/CompetitvePractice/HackerRank/ProjectEuler/CountingSundays.java
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/ProjectEuler/CountingSundays.java
@@ -65,11 +65,7 @@ public class CountingSundays {
 	public static boolean isLeapYear(int year) {
 		if (year % 4 == 0) {
 			if (year % 100 == 0) {
-				if (year % 400 == 0) {
-					return true;
-				} else {
-					return false;
-				}
+				return year % 400 == 0;
 			} else {
 				return true;
 			}
