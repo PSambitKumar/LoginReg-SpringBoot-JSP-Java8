4de041866400eaa4523e1c2947d27381fb7fc9b0
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-10-22 10:31:05 +0530

    Java Hackerrank Solution

---
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/Java/JavaPrimalityTest.java
@@ -0,0 +1,26 @@
+package com.sambit.CompetitvePractice.HackerRank.Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 21/10/2022 - 11:44 AM
+ */
+public class JavaPrimalityTest {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		String n = bufferedReader.readLine();
+		BigInteger y = new BigInteger(n);
+		if(y.isProbablePrime(1)){
+			System.out.println("prime");
+		}else{
+			System.out.println("not prime");
+		}
+		bufferedReader.close();
+	}
+}
--- src/main/java/com/sambit/CompetitvePractice/HackerRank/ProjectEuler/CountingSundays.java
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/ProjectEuler/CountingSundays.java
@@ -65,7 +65,11 @@ public class CountingSundays {
 	public static boolean isLeapYear(int year) {
 		if (year % 4 == 0) {
 			if (year % 100 == 0) {
-				return year % 400 == 0;
+				if (year % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
 			} else {
 				return true;
 			}
