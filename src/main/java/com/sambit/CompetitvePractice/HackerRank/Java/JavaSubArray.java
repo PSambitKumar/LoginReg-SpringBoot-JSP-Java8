975c0f49709e45269d6772c9fcbdb50e6ccd58b0
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-10-29 22:04:25 +0530

    Java Sub Array

---
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/Java/JavaSubArray.java
@@ -0,0 +1,30 @@
+package com.sambit.CompetitvePractice.HackerRank.Java;
+
+import java.util.Scanner;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 29/10/2022 - 10:03 PM
+ */
+public class JavaSubArray {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] a = new int[n];
+		for (int a_i = 0; a_i < n; a_i++) {
+			a[a_i] = in.nextInt();
+		}
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			int sum = 0;
+			for (int j = i; j < n; j++) {
+				sum += a[j];
+				if (sum < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
