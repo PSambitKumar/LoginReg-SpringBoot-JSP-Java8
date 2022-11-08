13466d2c3efb76abcaad0e5ed57759e27b38fd66
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-11-08 15:18:30 +0530

    Currency Convrter File Created

---
+++ src/main/java/com/sambit/Utils/CurrencyConverter.java
@@ -0,0 +1,30 @@
+package com.sambit.Utils;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 08/11/2022 - 3:02 PM
+ */
+public class CurrencyConverter {
+
+//	Input: "123456789", Output : "123,456,789"
+	public static String indianCurrencyFormat(String rupees){
+		String string = (new StringBuilder((new StringBuilder(rupees).reverse().toString())
+			   .replaceAll("(.{3})", "$1,")).reverse().toString())
+			   .replaceAll("^,|,$", "");
+		System.out.println(string);
+		return string;
+	}
+
+//	Input : "123456789", Output : "₹123,456,789.00"
+	public static String currencyFormat(String rupees, String countryCode){
+		NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("en", countryCode));
+		String currency = format.format(Double.parseDouble(rupees));
+		System.out.println(currency);
+		return currency;
+	}
+
+}
