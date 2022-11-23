3633dbc72ff73baffe7882e2b1f8ee8717d84ad5
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-11-23 19:34:15 +0530

    Abstract Class

---
+++ src/main/java/com/sambit/CompetitvePractice/HackerRank/Java/JavaAbstractClass.java
@@ -0,0 +1,35 @@
+package com.sambit.CompetitvePractice.HackerRank.Java;
+
+import java.util.Scanner;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 23/11/2022 - 7:32 PM
+ */
+abstract class Book{
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+}
+
+class MyBook extends Book{
+    @Override
+    void setTitle(String s) {
+        title = s;
+    }
+}
+
+public class JavaAbstractClass {
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        String title=sc.nextLine();
+        MyBook new_novel=new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: "+new_novel.getTitle());
+        sc.close();
+
+    }
+}
