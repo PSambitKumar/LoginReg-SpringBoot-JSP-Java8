1bc9ec1501a1ed34ba1f4bb405c86dfaba4a2a2d
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-12-01 09:48:11 +0530

    Java Method Overloading with super keyword

--- src/main/java/com/sambit/Controller/RegController.java
+++ src/main/java/com/sambit/Controller/RegController.java
@@ -23,6 +23,8 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
@@ -596,7 +598,11 @@ public class RegController {
         regService.generateRegistrationPDFReport(httpServletResponse);
     }
 
-
-
-
+    @GetMapping(value = "storeDataInHash")
+    public void storeDataInHash(){
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("Name", "Sambit");
+        map.add("Name", "Sambit");
+        System.out.println("Map Data : " + map);
+    }
 }
