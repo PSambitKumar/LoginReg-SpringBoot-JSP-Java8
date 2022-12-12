c9857d2781fe093c69a417e5b91805d9db267851
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2022-12-12 17:26:28 +0530

    Map Practice

---
+++ src/main/java/com/sambit/CompetitvePractice/CollectionFramework/Map.java
@@ -0,0 +1,19 @@
+package com.sambit.CompetitvePractice.CollectionFramework;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 12/12/2022 - 5:25 PM
+ */
+public class Map {
+    public static void main(String[] args) {
+        java.util.Map<String, String> map = new java.util.HashMap<>();
+        map.put("id", "1");
+        map.put("name", "Sambit");
+        map.put("age", "26");
+
+        for (java.util.Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+}
