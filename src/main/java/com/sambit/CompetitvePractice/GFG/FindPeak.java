16a5ee0623620b6647aa8f50044e5f965a46d332
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-01-18 18:07:06 +0530

    Changes

---
+++ src/main/java/com/sambit/CompetitvePractice/GFG/FindPeak.java
@@ -0,0 +1,13 @@
+package com.sambit.CompetitvePractice.GFG;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 18/01/2023 - 5:36 PM
+ */
+public class FindPeak {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0};
+//        System.out.println(findPeak(arr));
+    }
+}
