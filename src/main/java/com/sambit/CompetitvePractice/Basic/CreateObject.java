0267027d05c0400f854c316bfc9507c4ff3a5341
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-03-03 18:03:01 +0530

    Create Object

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/CreateObject.java
@@ -0,0 +1,16 @@
+package com.sambit.CompetitvePractice.Basic;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 03/03/2023 - 6:01 PM
+ */
+public class CreateObject {
+    void show() {
+        System.out.println("Welcome Sambit Kumar Pradhan");
+    }
+    public static void main(String[] args) {
+        CreateObject obj = new CreateObject();
+        obj.show();
+    }
+}
