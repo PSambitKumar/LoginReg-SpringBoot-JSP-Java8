ce9d2b58a8f0c2301f8c7df3406bfeb72739c1c6
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-03-07 22:09:56 +0530

    Testing Array

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/TestArray.java
@@ -0,0 +1,18 @@
+package com.sambit.CompetitvePractice.Basic;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 07/03/2023 - 10:08 PM
+ */
+class Testarray{
+    public static void main(String[] args){
+        int[] a =new int[5];
+        a[0]=10;
+        a[1]=20;
+        a[2]=70;
+        a[3]=40;
+        a[4]=50;
+        for (int j : a) System.out.println(j);
+    }
+}
