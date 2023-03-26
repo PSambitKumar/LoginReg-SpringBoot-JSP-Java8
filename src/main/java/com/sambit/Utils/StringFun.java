e9aaafec3ce033946222a564b07f6d44a6cd3ff5
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-03-26 23:06:21 +0530

    String function

---
+++ src/main/java/com/sambit/Utils/StringFun.java
@@ -0,0 +1,36 @@
+package com.sambit.Utils;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 26/03/2023 - 11:04 PM
+ */
+public class StringFun {
+    public static String[] splitString(String str, String delimiter){
+        return str.split(delimiter);
+    }
+
+    public static String[] splitString(String str){
+        return str.split(" ");
+    }
+
+    public static String[] splitString(String str, String delimiter, int limit){
+        return str.split(delimiter, limit);
+    }
+
+    public static String[] splitString(String str, int limit){
+        return str.split(" ", limit);
+    }
+
+    public static String[] splitString(String str, String delimiter, int limit, int offset){
+        return str.split(delimiter, limit);
+    }
+
+    public static void main(String[] args) {
+        String str = "Sambit Kumar Pradhan";
+        String[] strArray = splitString(str);
+        for (String s : strArray) {
+            System.out.println(s);
+        }
+    }
+}
