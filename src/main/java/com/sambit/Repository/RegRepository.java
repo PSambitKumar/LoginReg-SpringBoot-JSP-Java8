cc7d8fa64029c3df4262c64fdcd47c384441ab58
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-04-04 16:05:17 +0530

    Dynamic Query For Study

--- src/main/java/com/sambit/Repository/RegRepository.java
+++ src/main/java/com/sambit/Repository/RegRepository.java
@@ -4,6 +4,7 @@ import com.sambit.Entity.Reg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface RegRepository extends JpaRepository<Reg, Integer> {
@@ -44,4 +45,31 @@ public interface RegRepository extends JpaRepository<Reg, Integer> {
 
 //    @Query(value = "FROM MobileVersion mv WHERE mv.id = (SELECT MAX(mv1.id) FROM MobileVersion mv1)")
 //    MobileVersion getMobileVersionByMaxId();
+
+    @Query(value = "SELECT\n" +
+            "    H.HOSPITAL_NAME,\n" +
+            "    H.STATE_CODE,\n" +
+            "    S.STATENAME,\n" +
+            "    H.DISTRICT_CODE,\n" +
+            "    D.DISTRICTNAME,\n" +
+            "    H.HOSPITALTYPE,\n" +
+            "    H.HOSPITALPINCODE,\n" +
+            "    H.HOSPITALADDRESS,\n" +
+            "    H.CONTACTPERSON,\n" +
+            "    H.EMAIL_ID,\n" +
+            "    H.LATITUDE,\n" +
+            "    H.LONGITUDE,\n" +
+            "    CASE\n" +
+            "        WHEN H.STATUS_FLAG = 0\n" +
+            "            THEN 'ACTIVE'\n" +
+            "        ELSE 'INACTIVE'\n" +
+            "    END AS STATUS_FLAG\n" +
+            "FROM HOSPITAL_INFO H\n" +
+            "INNER JOIN STATE S ON H.STATE_CODE = S.STATECODE\n" +
+            "INNER JOIN DISTRICT D ON S.STATECODE = D.STATECODE AND H.DISTRICT_CODE = D.DISTRICTCODE\n" +
+            "WHERE H.STATUS_FLAG = 0\n" +
+            "AND ((?2 = 'I' AND TRUNC(H.CREATED_ON) = ?1)\n" + //?1 = date, ?2 = flag (I/U) Important for Study Dynamic Query
+            "OR (?2 = 'U' AND TRUNC(H.UPDATED_ON) = ?1))\n" +
+            "ORDER BY H.HOSPITAL_NAME", nativeQuery = true)
+    List<Object[]> getHospitalList(Date date, char flag);
 }
