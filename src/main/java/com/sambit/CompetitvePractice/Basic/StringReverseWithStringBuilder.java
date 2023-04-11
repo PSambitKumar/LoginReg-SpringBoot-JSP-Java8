6c20e8b7ace8b1ca8023377bea85788e3ca4421a
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-04-11 17:00:39 +0530

    String reverse

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/StringReverseWithStringBuilder.java
@@ -0,0 +1,16 @@
+package com.sambit.CompetitvePractice.Basic;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 11/04/2023 - 4:59 PM
+ */
+public class StringReverseWithStringBuilder {
+    public static void main(String[] args) {
+        String str = "Automation";
+        StringBuilder str2 = new StringBuilder();
+        str2.append(str);
+        str2.reverse();
+        System.out.println(str2);
+    }
+}
