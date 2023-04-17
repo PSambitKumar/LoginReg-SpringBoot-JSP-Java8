abbb023b1947de4903d36d971ff75151d936f8fe
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-04-17 10:53:42 +0530

    generate Dynamic Query

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/DynamicQueryForARangeOfList.java
@@ -0,0 +1,35 @@
+package com.sambit.CompetitvePractice.Basic;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 17/04/2023 - 10:20 AM
+ */
+public class DynamicQueryForARangeOfList {
+    public static String generateDynamicQuery(int pageNo, int pageSize) {
+        int start = (pageNo - 1) * pageSize;
+        int end = pageNo * pageSize;
+
+        System.out.println("Start: " + start + " End: " + end);
+
+        return "SELECT * FROM (SELECT ROWNUM AS ROWNUMBER, A.* FROM (SELECT * FROM TABLE_NAME) A WHERE ROWNUM <= " + end + ") WHERE ROWNUMBER > " + start;
+    }
+
+    public static String generateDynamicQuery1(int pageNo, int pageSize) {
+        int x = pageNo - 1;
+        int start = (pageSize * x) + 1;
+        int end = pageSize * pageNo;
+
+        System.out.println("Start: " + start + " End: " + end);
+
+        return "SELECT * FROM (SELECT ROWNUM AS ROWNUMBER, A.* FROM (SELECT * FROM TABLE_NAME) A WHERE ROWNUM <= " + end + ") WHERE ROWNUMBER > " + start;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateDynamicQuery(1, 10));
+        System.out.println(generateDynamicQuery1(1, 10));
+
+        System.out.println(generateDynamicQuery(2, 10));
+        System.out.println(generateDynamicQuery1(2, 10));
+    }
+}
