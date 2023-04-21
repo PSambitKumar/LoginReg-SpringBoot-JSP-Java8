d9b51bcd5e7fdfa8d7b23166330d039174bbb1cf
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-04-21 15:04:38 +0530

    Changes

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/dATA.java
@@ -0,0 +1,31 @@
+package com.sambit.CompetitvePractice.Basic;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 21/04/2023 - 12:42 PM
+ */
+public class dATA {
+
+    public static void main(String[] args) {
+        List<String> datas = Arrays.asList("1524","707","1140","706","699","905","840","720","866","865","826","823","820","816","901","891","881","911","886","910", "12312", "2934");
+        System.out.println(datas.size());
+
+//        Print 10 data batchwise as stringBuffer
+        int count = 0;
+        StringBuffer stringBuffer = new StringBuffer();
+        for (String data : datas) {
+            stringBuffer.append(data).append(",");
+            count++;
+            if (count == 10) {
+                System.out.println(stringBuffer);
+                stringBuffer = new StringBuffer();
+                count = 0;
+            }
+        }
+        System.out.println(stringBuffer);
+    }
+}
