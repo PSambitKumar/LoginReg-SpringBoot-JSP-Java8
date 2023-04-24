fbb8adf7a18c79c887ea2a806d1e8bb164e4373a
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-04-24 23:23:13 +0530

    CHnages

--- src/main/java/com/sambit/CompetitvePractice/Basic/TestJava.java
+++ src/main/java/com/sambit/CompetitvePractice/Basic/TestJava.java
@@ -1,5 +1,9 @@
 package com.sambit.CompetitvePractice.Basic;
 
+import com.google.gson.JsonArray;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +12,9 @@ import java.util.List;
  * @Auther : Sambit Kumar Pradhan
  * @Created On : 21/04/2023 - 12:42 PM
  */
-public class dATA {
+public class TestJava {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JSONException {
         List<String> datas = Arrays.asList("1524","707","1140","706","699","905","840","720","866","865","826","823","820","816","901","891","881","911","886","910", "12312", "2934");
         System.out.println(datas.size());
 
@@ -27,5 +31,21 @@ public class dATA {
             }
         }
         System.out.println(stringBuffer);
+
+
+        StringBuilder stringBuilder1 = new StringBuilder();
+        stringBuilder1.append("SELECT ");
+
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.put("Sambit");
+        jsonArray.put("Kumar");
+        jsonArray.put("Pradhan");
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            System.out.println(jsonArray.get(i));
+            stringBuilder1.append(jsonArray.get(i)).append(",");
+        }
+
+        System.out.println(stringBuilder1);
     }
 }
