59aff512461f66fbfa9f1f45c6bca010e2296702
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-05-11 18:11:42 +0530

    Chnages

---
+++ src/main/java/com/sambit/Config/LoggerConfig.java
@@ -0,0 +1,19 @@
+package com.sambit.Config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Project : Registration
+ * @Auther : Sambit Kumar Pradhan
+ * @Created On : 10/05/2023 - 9:20 AM
+ */
+@Configuration
+public class LoggerConfig {
+    @Bean
+    public Logger getLogger() {
+        return LoggerFactory.getLogger(LoggerConfig.class);
+    }
+}
