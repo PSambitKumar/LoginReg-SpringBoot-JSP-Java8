7c3c4f90c4d2eb6f697f9f64ea5438d72dd7352c
Sambti Kumar Pradhan <sambitkumarpradhan8@gmail.com>
2023-05-21 23:38:36 +0530

    Birth Year

--- src/main/java/com/sambit/Utils/AgeCalculator.java
+++ src/main/java/com/sambit/Utils/AgeCalculator.java
@@ -33,4 +33,28 @@ public class AgeCalculator {
 	public static int calculateAge(String birthYear, String currentYear) {
 		return Integer.parseInt(currentYear) - Integer.parseInt(birthYear);
 	}
+
+	public static int calculateAge(String birthYear, int currentYear) {
+		return currentYear - Integer.parseInt(birthYear);
+	}
+
+	public static int calculateAge(int birthYear, String currentYear) {
+		return Integer.parseInt(currentYear) - birthYear;
+	}
+
+	public static int calculateAge(LocalDate birthDate) {
+		return Period.between(birthDate, LocalDate.now()).getYears();
+	}
+
+	public static int calculateAge(String birthDate) {
+		return Period.between(LocalDate.parse(birthDate), LocalDate.now()).getYears();
+	}
+
+	public static int calculateAge(int birthDate) {
+		return Period.between(LocalDate.of(birthDate, 1, 1), LocalDate.now()).getYears();
+	}
+
+	public static int calculateAge(String birthDate, LocalDate currentDate) {
+		return Period.between(LocalDate.parse(birthDate), currentDate).getYears();
+	}
 }
