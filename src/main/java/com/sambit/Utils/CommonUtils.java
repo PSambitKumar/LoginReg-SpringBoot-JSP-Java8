7ac7ea8c5625e74f80076a113a82c81031313813
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-06-22 00:16:27 +0530

    String to MapList Method addrd

--- src/main/java/com/sambit/Utils/CommonUtils.java
+++ src/main/java/com/sambit/Utils/CommonUtils.java
@@ -3,8 +3,12 @@ package com.sambit.Utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.sql.Clob;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +39,23 @@ public class CommonUtils {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             return objectMapper.readValue(data, new TypeReference<List<Map<String, Object>>>() {});
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static List<Map<String, Object>> convertStringToListMap1(String data) {
+        try {
+            JSONArray jsonArray = new JSONArray(data);
+            List<Map<String, Object>> list = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Map<String, Object> map = new LinkedHashMap<>();
+                map.put("id", jsonObject.get("id"));
+                map.put("name", jsonObject.get("name"));
+                list.add(map);
+            }
+            return list;
         } catch (Exception e) {
             return null;
         }
