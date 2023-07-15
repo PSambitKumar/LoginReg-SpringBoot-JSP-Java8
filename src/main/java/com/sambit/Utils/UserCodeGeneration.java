f16873ece0163413b798a14cd2de1d2f9d37f869
Sambit Kumar Pradhan <sambitkumarpradhan8@gmail.com>
2023-07-15 22:18:17 +0530

    UseCodeGneration Method

--- src/main/java/com/sambit/Utils/UserCodeGeneration.java
+++ src/main/java/com/sambit/Utils/UserCodeGeneration.java
@@ -11,4 +11,15 @@ public class UserCodeGeneration {
     public static String userCodeGenerationMethod(){
         return "USER" + String.format("%08d", new Random().nextInt(99999999));
     }
+
+    public static String userCodeGenerationMethod(int maxUserSlNo){
+        return "USER" + String.format("%08d", maxUserSlNo + 1);
+    }
+
+    public static String userCodeGenerationMethod(String maxUserSlNo){
+        return "USER" + String.format("%08d", Integer.parseInt(maxUserSlNo) + 1);
+    }
+    public static void main(String[] args) {
+        System.out.println(userCodeGenerationMethod());
+    }
 }
