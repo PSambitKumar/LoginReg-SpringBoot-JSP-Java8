e2ff036757becf5d34d966ac51add1c4b724f50c
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-07-21 23:32:04 +0530

    Chnages in Tech Number

--- src/main/java/com/sambit/CompetitvePractice/Basic/TechNumber.java
+++ src/main/java/com/sambit/CompetitvePractice/Basic/TechNumber.java
@@ -7,6 +7,18 @@ package com.sambit.CompetitvePractice.Basic;
  */
 public class TechNumber {
 
+    public static boolean isTechNumber(int num) {
+        int temp = num;
+        int sum = 0;
+        while (temp > 0) {
+            int rem = temp % 10;
+            sum += rem;
+            temp /= 10;
+        }
+        if (num % sum == 0) return true;
+        else return false;
+    }
+
     public static void main(String[] args) {
         int num = 123;
         int temp = num;
