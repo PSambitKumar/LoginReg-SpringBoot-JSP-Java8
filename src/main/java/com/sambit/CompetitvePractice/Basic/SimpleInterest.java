37f350b1206e8372328cac7effe788f43d8f9e34
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-08-01 12:38:00 +0530

    AES Encryption

--- src/main/java/com/sambit/CompetitvePractice/Basic/SimpleInterest.java
+++ src/main/java/com/sambit/CompetitvePractice/Basic/SimpleInterest.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
  * @Author : Sambit Kumar Pradhan
  * @Created On : 30/07/2023 - 11:45 PM
  */
-public class SimpleIntrest {
+public class SimpleInterest {
 
-    public static void calculateSimpleIntrest() {
+    public static void calculateSimpleInterest() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Principal Amount: ");
         int principalAmount = sc.nextInt();
@@ -17,11 +17,11 @@ public class SimpleIntrest {
         float rateOfInterest = sc.nextFloat();
         System.out.println("Enter the Time Period: ");
         int timePeriod = sc.nextInt();
-        float simpleIntrest = (principalAmount * rateOfInterest * timePeriod) / 100;
-        System.out.println("Simple Intrest is: " + simpleIntrest);
+        float simpleInterest = (principalAmount * rateOfInterest * timePeriod) / 100;
+        System.out.println("Simple Interest is: " + simpleInterest);
     }
     public static void main(String[] args) {
-        calculateSimpleIntrest();
+        calculateSimpleInterest();
 
     }
 }
