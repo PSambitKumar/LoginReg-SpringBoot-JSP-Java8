0ff78cca467c3efa84f7add5b85e6d22b6c009c1
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-11-10 15:23:30 +0530

    Generate Email Body For CPD Allotment

--- src/main/java/com/sambit/Utils/WhatsAppSender.java
+++ src/main/java/com/sambit/Utils/WhatsAppSender.java
@@ -3,14 +3,17 @@ package com.sambit.Utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import okhttp3.*;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -145,5 +148,72 @@ public class WhatsAppSender {
             System.out.println("Exception" + e);
         }
         return statusCode;
+    }
+
+    public int sendWhatsAppMessage(String phoneNumber, String bodyText, String templateName) {
+        int statusCode = 500;
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("action", "Action")
+                .addFormDataPart("phone", "+91" + phoneNumber)
+                .addFormDataPart("template_name", templateName)
+                .addFormDataPart("body_text", bodyText)
+                .build();
+
+
+        Request request = new Request.Builder()
+                .url("URL")
+                .method("POST", body)
+                .build();
+
+//        client.newBuilder().readTimeout(180, TimeUnit.SECONDS);// set read timeout
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                String responseBody = response.body().string();
+                Map<?, ?> responseMap = new ObjectMapper().readValue(responseBody, Map.class);
+                statusCode = Integer.parseInt(responseMap.get("status").toString()) == 1 ? 200 : 500;
+            } else {
+                System.out.println("Invalid response");
+            }
+        } catch (IOException e) {
+            System.out.println("Exception" + e);
+        }
+        return statusCode;
+    }
+
+        public static String generateEmailBodyForCPDAllotment(String name, List<Map<String, Object>> cpdAssignmentLogMapList, List<String> headersList) {
+        StringBuilder emailBody = new StringBuilder();
+
+        emailBody.append("Dear ").append(name).append(",\n\n")
+                .append("CPD claims allotment report has been generated.\n")
+                .append("This report contains the list of the claims that have been allotted to you.\n\n");
+
+        // Add the table header
+        emailBody.append("<table border=\"1\" cellpadding=\"5\" cellspacing=\"0\">");
+        emailBody.append("<tr>");
+        for (String header : headersList) {
+            emailBody.append("<th>").append(header).append("</th>");
+        }
+        emailBody.append("</tr>");
+
+        // Add the data rows
+        for (Map<String, Object> cpdAssignmentLogMap : cpdAssignmentLogMapList) {
+            emailBody.append("<tr>");
+            for (String header : headersList) {
+                emailBody.append("<td>").append(cpdAssignmentLogMap.get(header)).append("</td>");
+            }
+            emailBody.append("</tr>");
+        }
+
+        // Close the table
+        emailBody.append("</table>\n\n");
+
+        emailBody.append("Regards,\n")
+                .append("BSKY Team\n\n")
+                .append("This is a system-generated mail. Please do not reply.\n\n");
+
+        return emailBody.toString();
     }
 }
