377be9cf1bb035bfe32a25d203087f379af9dbd1
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2024-01-08 11:49:28 +0530

    Java 8 Stream List filter inside a list loop

--- src/main/java/com/sambit/Service/RegServiceImpl.java
+++ src/main/java/com/sambit/Service/RegServiceImpl.java
@@ -34,6 +34,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.List;
