48886211deeb3610179bd9ef225dbe557f204dfb
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2024-04-30 18:22:14 +0530

    Reverse an integer Array

---
+++ src/main/java/com/sambit/CompetitvePractice/Basic/ReverseIntArr.java
@@ -0,0 +1,54 @@
+package com.sambit.CompetitvePractice.Basic;
+
+import java.util.*;
+
+/**
+ * @Project : Registraion
+ * @Author: Sambit Kumar Pradhan
+ * @Date: 30-Apr-2024 : 6:12 PM
+ */
+
+public class ReverseIntArr {
+
+    ReverseIntArr(){}
+
+    public static int[] reverseArr(int[] arr) {
+        Integer[] boxedArray = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        List<Integer> intList = Arrays.asList(boxedArray);
+        Collections.reverse(intList);
+        return intList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void reverseArr1(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[length - i - 1];
+            arr[length - i - 1] = temp;
+        }
+    }
+
+    public static int[] reverseArr2(int[] arr) {
+        int length = arr.length;
+        int[] reversedArr = new int[length];
+        for (int i = 0; i < length; i++) {
+            reversedArr[length - i - 1] = arr[i];
+        }
+        return reversedArr;
+    }
+
+    public static int[] reverseArr3(int[] arr) {
+        int length = arr.length;
+        int[] reversedArr = new int[length];
+        int i = 0;
+        int j = length - 1;
+
+        while (i < length) {
+            reversedArr[i] = arr[j];
+            i++;
+            j--;
+        }
+
+        return reversedArr;
+    }
+}
