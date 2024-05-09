1460ff90bfe68e407a564f8bebc01ac99e6d22d6
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2024-05-09 12:10:43 +0530

    PrintEvenWordsOfAString

--- src/main/java/com/sambit/CompetitvePractice/StringManupulation/PrintEvenWordsOfAString.java
+++ src/main/java/com/sambit/CompetitvePractice/StringManupulation/PrintEvenWordsOfAString.java
@@ -11,12 +11,16 @@ import java.util.List;
 
 public class PrintEvenWordsOfAString {
 
+//    Driver Code
     public static void main(String[] args) {
         String str = "Java is a programming language.";
 
-        printEvenWordOfAString(str);
+        printEvenWordOfAString("Java is a programming language.");
+        printEvenWordOfAString1("Sambit Kumar Pradhan is a student.");
+        printEvenWordOfAString2("Even lengths words printing.");
     }
 
+//    Logics
     private static void printEvenWordOfAString(String string) {
         string = string.replaceAll("\\.", "");
         String[] strArr = string.split(" ");
@@ -28,6 +32,26 @@ public class PrintEvenWordsOfAString {
     }
 
     private static void printEvenWordOfAString1(String string) {
-        List<String> listString = new ArrayList<>();
+        string = string.replaceAll("\\.", "");
+        for (String str : string.split(" ")) {
+            if (str.length() % 2 == 0)
+                System.out.println(str);
+        }
+    }
+
+    private static void printEvenWordOfAString2(String string) {
+        string = string.replaceAll("\\.", "");
+        String[] strings = string.split(" ");
+
+        int[] sizes = new int[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            sizes[i] = strings[i].length();
+        }
+
+        for (int i = 0; i < sizes.length; i++) {
+            if ((sizes[i] % 2) == 0)
+                System.out.println(strings[i]);
+        }
     }
 }
