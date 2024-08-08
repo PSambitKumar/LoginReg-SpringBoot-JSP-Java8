cd4e5b19a4d93e9cf27f09ab6d289866220f720e
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2024-08-08 11:41:48 +0530

    List Operations

--- src/main/java/com/sambit/CompetitvePractice/Java8/Java8Practice.java
+++ src/main/java/com/sambit/CompetitvePractice/Java8/Java8Practice.java
@@ -168,6 +168,101 @@ public class Java8Practice {
         common.retainAll(integers2);
     }
 
+    public static void listOperation() {
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream()
+                .map( i -> i*i)
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany",
+                "Italy", "U.K.","Canada");
+        String G7Countries = G7.stream()
+                .map(x -> x.toUpperCase())
+                .collect(Collectors.joining(", "));
+
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        long count = strList.stream()
+                .filter(x -> x.isEmpty())
+                .count();
+
+        List<String> filtered = strList.stream()
+                .filter(x -> x.length()> 2)
+                .collect(Collectors.toList());
+
+        List<String> filtered1 = strList.stream()
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.toList());
+
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream()
+                .mapToInt((x) -> x)
+                .summaryStatistics();
+
+        // Count the empty strings
+        List<String> strList1 = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        long count1 = strList1.stream()
+                .filter(x -> x.isEmpty())
+                .count();
+        System.out.printf("List %s has %d empty strings %n", strList, count1);
+
+        // Count String with length more than 3
+        long num = strList1.stream()
+                .filter(x -> x.length()> 3)
+                .count();
+        System.out.printf("List %s has %d strings of length more than 3 %n",
+                strList1, num);
+
+
+        // Count number of String which startswith "a"
+        count = strList.stream()
+                .filter(x -> x.startsWith("a"))
+                .count();
+        System.out.printf("List %s has %d strings which startsWith 'a' %n",
+                strList, count);
+
+        // Remove all empty Strings from List
+        List<String> filtered2 = strList.stream()
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.toList());
+        System.out.printf("Original List : %s, List without Empty Strings : %s %n",
+                strList, filtered2);
+
+        // Create a List with String more than 2 characters
+        filtered2 = strList.stream()
+                .filter(x -> x.length()> 2)
+                .collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n",
+                strList, filtered2);
+
+
+        // Convert String to Uppercase and join them using coma
+        List<String> G71 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy",
+                "U.K.","Canada");
+        String G7Countries1 = G71.stream()
+                .map(x -> x.toUpperCase())
+                .collect(Collectors.joining(", "));
+        System.out.println(G7Countries1);
+
+        // Create List of square of all distinct numbers
+        List<Integer> numbers1 = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct1 = numbers1.stream()
+                .map( i -> i*i).distinct()
+                .collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n",
+                numbers, distinct1);
+
+        //Get count, min, max, sum, and average for numbers
+        List<Integer> primes1 = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats1 = primes1.stream()
+                .mapToInt((x) -> x)
+                .summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats1.getMax());
+        System.out.println("Lowest prime number in List : " + stats1.getMin());
+        System.out.println("Sum of all prime numbers : " + stats1.getSum());
+        System.out.println("Average of all prime numbers : " + stats1.getAverage());
+    }
+
     public static void main(String[] args) {
 //        findSumAnsAverage();
 //        extractOddAndEvenNumbers();
